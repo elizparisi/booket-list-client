e84c4eabc750bf15cbb5981863f5c28b650ae39f
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-13 21:50:28 -0400

    add redux and react-router-dom to package.json

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 
 class App extends React.Component {
 
+  componentDidMount(){
+    fetch('http://localhost:3000/api/v1/lists/1/books')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
   render(){
 
     return (
