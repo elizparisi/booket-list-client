6f55c2c5640e6d4e3039519633380c4c1db1b86d
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-14 17:23:09 -0400

    fix backend address in fetchLists action

--- src/actions/fetchLists.js
+++ src/actions/fetchLists.js
@@ -3,7 +3,7 @@ export function fetchLists(){
 
 // instead of connect automatically dispatching an action with thunk we can pass dispatch into our function and use it inside our action creator
   return (dispatch) => {
-    fetch("http://localhost:3000/lists")
+    fetch("http://localhost:3000/api/v1/lists")
     .then(response => response.json())
     .then(lists => dispatch({
       type: "FETCH_LISTS",
