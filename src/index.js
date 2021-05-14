77ff22492868de5f489c01d81ab6e19a225481b2
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-14 00:06:48 -0400

    update index.js with listReducer

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import bookReducer from './reducers/bookReducer'
+import listReducer from './reducers/listReducer'
 
 // takes in an action object, decides what we want to update in our store
-const rootReducer = combineReducers({
-  bookReducer
-})
+// const rootReducer = combineReducers({
+//   bookReducer
+// })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // stores our data
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+const store = createStore(listReducer, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
   // wrap App in Provider to give it access to Redux store
