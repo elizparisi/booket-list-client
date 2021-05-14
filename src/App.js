80adaefe8b5383cce60f0ef58194bc99e8231865
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-14 00:37:52 -0400

    import connect in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css';
+import { connect } from 'react-redux'
 
 class App extends React.Component {
 
   componentDidMount(){
-    fetch('http://localhost:3000/api/v1/lists/1/books')
+    fetch('http://localhost:3000/api/v1/lists')
     .then(response => response.json())
     .then(data => console.log(data))
   }
@@ -17,4 +18,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default connect()(App);
