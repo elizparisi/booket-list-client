6c2fc6ea874bf678f25b6b9893131e714c217fcc
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-14 16:36:57 -0400

    import fetchLists in ListsContainer

--- src/containers/ListsContainer.js
+++ src/containers/ListsContainer.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {fetchLists} from '../actions/fetchLists'
 import Lists from '../components/Lists'
 import ListInput from '../components/ListInput'
 
 class ListsContainer extends React.Component {
 
   componentDidMount() {
-    
+
   }
 
   render() {
