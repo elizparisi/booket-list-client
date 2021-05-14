2997eb7d2b43e8037f75c5b5bb64900939483eba
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-14 14:11:06 -0400

    add Lists + ListsInput components

---
+++ src/components/ListInput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class ListInput extends React.Component {
+
+  render() {
+    return(
+      <div>
+        ListInput
+      </div>
+    )
+  }
+}
+
+export default ListInput
