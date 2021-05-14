47e501019f52f5024d9ffeaad1478fa406dd0892
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-14 16:48:59 -0400

    build out fetchLists function with dispatch

--- src/actions/fetchLists.js
+++ src/actions/fetchLists.js
@@ -1,5 +1,13 @@
 
 export function fetchLists(){
 
-
+// instead of connect automatically dispatching an action with thunk we can pass dispatch into our function and use it inside our action creator
+  return (dispatch) => {
+    fetch("http://localhost:3000/lists")
+    .then(response => response.json())
+    .then(lists => dispatch({
+      type: "FETCH_LISTS",
+      payload: lists
+    }))
+  }
 }
