ed9e3aaafda044958669595764276ae8bbe50a2e
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-15 03:11:24 -0400

    set state + add value for controlled form

--- src/components/ListInput.js
+++ src/components/ListInput.js
@@ -2,12 +2,17 @@ import React from 'react'
 
 class ListInput extends React.Component {
 
+// set local state
+  state = {
+    name: ''
+  }
+
   render() {
     return(
       <div>
         <form>
           <label>List Name:</label>
-          <input type='text' placeholder='Enter your book list name'/>
+          <input type='text' placeholder='Enter your book list name' value={this.state.name}/>
         </form>
       </div>
     )
