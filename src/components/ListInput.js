30ca272efec17d3a388f4b8e67c219dc5e0bd624
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-15 00:53:19 -0400

    create uncontrolled form in ListInput component

--- src/components/ListInput.js
+++ src/components/ListInput.js
@@ -5,7 +5,10 @@ class ListInput extends React.Component {
   render() {
     return(
       <div>
-        ListInput
+        <form>
+          <label>List Name:</label>
+          <input type='text' placeholder='Enter your book list name'/>
+        </form>
       </div>
     )
   }
