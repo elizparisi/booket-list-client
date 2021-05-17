172eea854b391a2f4b29bfe00329b5fb826b7be2
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-17 18:08:11 -0400

    import BooksContainer + render it in the return

--- src/components/List.js
+++ src/components/List.js
@@ -1,15 +1,19 @@
 // individual list component that will show books associated with that list (stateless component)
 import React from 'react'
 import {Redirect} from 'react-router-dom'
+import BooksContainer from '../containers/BooksContainer'
 
 const List = (props) => {
 
   let list = props.lists[props.match.params.id - 1]
 
   return (
-    <li>
-      {list ? list.name : null}
-    </li>
+    <div>
+      <h2>
+        {list ? list.name : null}
+      </h2>
+      <BooksContainer/>
+    </div>
   )
 }
 
