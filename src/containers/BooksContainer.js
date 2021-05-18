8e30d14dd4ee024d1f4e13be9b1843793d614480
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-17 21:22:05 -0400

    pass Books component prop of books associated with a list

--- src/containers/BooksContainer.js
+++ src/containers/BooksContainer.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import BookForm from '../components/BookForm'
+import Books from '../components/Books'
 
 class BooksContainer extends React.Component {
 
   render() {
     return (
       <div>
-        BooksContainer
+        <BookForm/>
+        <Books books={this.props.list && this.props.list.books}/>
       </div>
     )
   }
