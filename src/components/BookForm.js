b70ada882ee503755f24a66f3bdfd8da0207c5a7
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-18 03:03:45 -0400

    add names + values to form

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -3,20 +3,29 @@ import {connect} from 'react-redux'
 
 class BookForm extends React.Component {
 
+  // set local state
+  state = {
+    title: '',
+    author: '',
+    image_url: '',
+    read: false,
+    rating: ''
+  }
+
   render() {
     return (
       <div>
         <form>
-          <label>Book Title:</label>
-          <input type='text' />
+          <label>Title:</label>
+          <input type='text' name='title' value={this.state.title}/>
           <label>Author:</label>
-          <input type='text' />
+          <input type='text' name='author' value={this.state.author}/>
           <label>Image URL:</label>
-          <input type='text' />
+          <input type='text' name='image_url' value={this.state.image_url}/>
           <label>Read?:</label>
-          <input type='checkbox' />
+          <input type='checkbox' name='read' value={this.state.read}/>
           <label>Rating:</label>
-          <input type='text' />
+          <input type='text' name='rating' value={this.state.rating}/>
         </form>
       </div>
     )
