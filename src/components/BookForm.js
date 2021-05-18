a18ad59df7e29445e5270e8d7a449872505762a0
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-18 02:32:23 -0400

    import + set up connect

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 class BookForm extends React.Component {
 
   render() {
     return (
       <div>
-      
+
       </div>
     )
   }
 }
 
-export default BookForm
+export default connect()(BookForm)
