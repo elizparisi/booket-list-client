2723fb8287d321faf24812008c7c5d3f12713680
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-18 02:30:05 -0400

    map over props and render book info

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,7 +4,9 @@ const Books = (props) => {
 
   return (
     <div>
-
+      {props.books && props.books.map(book =>
+        <li key={book.id}><img src={book.image_url} height='250' width='200'/> <br/> {book.title} {book.author} {book.read} {book.rating}</li>
+      )}
     </div>
   )
 }
