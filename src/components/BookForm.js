6ecd52ce7616225df84a3d8c1423f3397617b3ff
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-18 02:55:57 -0400

    start form-not controlled

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -6,10 +6,21 @@ class BookForm extends React.Component {
   render() {
     return (
       <div>
-
+        <form>
+          <label>Book Title:</label>
+          <input type='text' />
+          <label>Author:</label>
+          <input type='text' />
+          <label>Image URL:</label>
+          <input type='text' />
+          <label>Read?:</label>
+          <input type='checkbox' />
+          <label>Rating:</label>
+          <input type='text' />
+        </form>
       </div>
     )
   }
 }
 
-export default connect()(BookForm)
+export default connect(null, )(BookForm)
