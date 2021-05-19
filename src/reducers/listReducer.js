e94d1f1cfedcb4ec8f0aa4a9d5e80417a3d6895a
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-19 18:13:43 -0400

    add DELETE_BOOK case statement to listReducer

--- src/reducers/listReducer.js
+++ src/reducers/listReducer.js
@@ -18,6 +18,16 @@ export default function listReducer(state = {lists: []}, action){
       })
       return {...state, lists: lists}
 
+    case 'DELETE_BOOK':
+      let listsUpdate = state.lists.map(list => {
+        if (list.id === action.payload.id) {
+          return action.payload
+        } else {
+          return list
+        }
+      })
+      return {...state, lists: listsUpdate}
+
     default:
       return state
   }
