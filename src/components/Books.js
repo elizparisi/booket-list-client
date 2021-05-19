b3db8a31197c1cbb064e1910b4bbd30d2dcfc1e8
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-19 14:58:35 -0400

    add button to delete a book

--- src/components/Books.js
+++ src/components/Books.js
@@ -5,7 +5,7 @@ const Books = (props) => {
   return (
     <div>
       {props.books && props.books.map(book =>
-        <li key={book.id}><img src={book.image_url} height='250' width='200'/> <br/> {book.title} {book.author} {book.read} {book.rating}</li>
+        <li key={book.id}><img src={book.image_url} height='250' width='200'/> <br/> {book.title} {book.author} {book.read} {book.rating} <button>Delete</button></li>
       )}
     </div>
   )
