3d152d840087e1d69e2217d3744ee5f5424fa09c
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-20 01:12:56 -0400

    create Home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+
+  return (
+    <div>
+      <h1>Welcome to Booket Lists</h1>
+      <h2>Create your must read lists!</h2>
+    </div>
+  )
+}
+
+export default Home
