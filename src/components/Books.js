1aa575cf7c5f3ec1623a2d5d0733d81541298d67
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-19 20:09:36 -0400

    handleDelete functioning

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,14 +4,14 @@ import {deleteBook} from '../actions/deleteBook'
 
 const Books = (props) => {
 
-  const handleDelete = () => {
-    props.deleteBook()
+  const handleDelete = (book) => {
+    props.deleteBook(book.id, book.list_id)
   }
 
   return (
     <div>
       {props.books && props.books.map(book =>
-        <li key={book.id}><img src={book.image_url} height='250' width='200'/> <br/> {book.title} {book.author} {book.read} {book.rating} <button>Delete</button></li>
+        <li key={book.id}><img src={book.image_url} height='250' width='200'/> <br/> {book.title} {book.author} {book.read} {book.rating} <button onClick={() => handleDelete(book)}>Delete</button></li>
       )}
     </div>
   )
