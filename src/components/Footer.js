24eb81c82a4ecfd97ca5235f2b8f6e270388879f
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-21 18:18:37 -0400

    remove list + list items from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,9 +11,7 @@ class Footer extends React.Component {
     return (
       <footer className="my-5 pt-5 text-muted text-center text-small">
         <p className="mb-1">Made with <span style={heartStyle}>&#9829;</span> by Liz</p>
-        <ul className="list-inline">
-          <li className="list-inline-item"><a href="https://github.com/">Github</a></li>
-        </ul>
+        <p className="list-inline-item"><a href="https://github.com/">Github</a></p>
       </footer>
     )
   }
