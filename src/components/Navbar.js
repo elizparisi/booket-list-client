b0478c7a3766e3c38664d1cc4735877d23e3dad1
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-21 18:19:53 -0400

    change navbar back to original code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-// const link = {
-//     width: '100px',
-//     padding: '12px',
-//     margin: '0 6px 6px',
-//     background: 'pink',
-//     textDecoration: "none",
-//     color: 'white'
-// }
 
 const Navbar = (props) => {
 
   return (
-    <div>
+    <div className='nav'>
     <Link to='/home'>Home</Link>
     <Link to='/lists'>Lists</Link>
     <Link to='/lists/new'>Create a List</Link>
