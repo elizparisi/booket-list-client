366ab5f0c4e3bb7b02423ba200b694dca6305f78
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-22 03:49:17 -0400

    removed container from navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,27 +1,25 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {Container, Navbar, Nav} from 'react-bootstrap'
+import {Navbar, Nav} from 'react-bootstrap'
 
 
 const Navigation = (props) => {
 
   return (
-    <Container>
-      <Navbar bg="light" variant="light">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">BOOKET LISTS</Navbar.Brand>
           <Nav className="mr-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/lists">Lists</Nav.Link>
             <Nav.Link href="/lists/new">Create New List</Nav.Link>
          </Nav>
       </Navbar>
-    </Container>
   )
 }
 
 export default Navigation
 
-// 
+//
 // <div className='nav'>
 // <Link to='/home'>Home</Link>
 // <Link to='/lists'>Lists</Link>
