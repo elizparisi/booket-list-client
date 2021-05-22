8403b7949b13ca32a778f464d950e1ccecbcc1c4
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-21 23:04:15 -0400

    add card bootstrap styling + imports

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {deleteBook} from '../actions/deleteBook'
+import {Button, Card, Container, Row, Col} from 'react-bootstrap'
 
 const Books = (props) => {
 
@@ -11,7 +12,22 @@ const Books = (props) => {
   return (
     <div>
       {props.books && props.books.map(book =>
-        <li key={book.id}><img src={book.image_url} height='250' width='200'/> <br/> {book.title} {book.author} {book.read} {book.rating} <button onClick={() => handleDelete(book)}>Delete</button></li>
+        <Container>
+          <Row>
+          <Col md={4}>
+        <Card key={book.id} className='mb-3' style={{ width: '18rem' }}>
+        <Card.Img src={book.image_url} height='250' width='200'/>
+        <Card.Body>
+        <Card.Title>{book.title}</Card.Title>
+        <Card.Text>{book.author} {book.read} {book.rating}
+        </Card.Text>
+        <Button variant='primary' onClick={() => handleDelete(book)}>Delete</Button>
+        </Card.Body>
+        </Card>
+        </Col>
+        </Row>
+        </Container>
+
       )}
     </div>
   )
