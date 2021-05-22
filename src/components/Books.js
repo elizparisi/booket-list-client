7b8b34aff9d175d98822339b44af51ab348e4b45
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-22 03:26:18 -0400

    cleaned up card text

--- src/components/Books.js
+++ src/components/Books.js
@@ -14,20 +14,19 @@ const Books = (props) => {
       {props.books && props.books.map(book =>
         <Container>
           <Row>
-          <Col md={4}>
-        <Card key={book.id} className='mb-3' style={{ width: '18rem' }}>
-        <Card.Img src={book.image_url} height='250' width='200'/>
-        <Card.Body>
-        <Card.Title>{book.title}</Card.Title>
-        <Card.Text>{book.author} {book.read} {book.rating}
-        </Card.Text>
-        <Button variant='primary' onClick={() => handleDelete(book)}>Delete</Button>
-        </Card.Body>
-        </Card>
-        </Col>
-        </Row>
+            <Col xs={6} md={4}>
+              <Card key={book.id} className='mb-3' style={{ width: '18rem' }}>
+              <Card.Img src={book.image_url} />
+              <Card.Body>
+              <Card.Title>{book.title}</Card.Title>
+              <Card.Text>By: {book.author} {book.read} {book.rating}
+              </Card.Text>
+              <Button variant='primary' onClick={() => handleDelete(book)}>Delete</Button>
+              </Card.Body>
+              </Card>
+            </Col>
+          </Row>
         </Container>
-
       )}
     </div>
   )
