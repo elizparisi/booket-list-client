b16ac57fecc3f7b1842d1edf025e3dcab07266b3
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-24 05:30:10 -0400

    fix footer and footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,9 @@ class Footer extends React.Component {
 
   render() {
     return (
-      <footer className="my-5 pt-5 text-muted text-center text-small">
-        <p className="mb-1">Made with <span style={heartStyle}>&#9829;</span> by Liz</p>
-        <p className="list-inline-item"><a href="https://github.com/">Github</a></p>
+      <footer className="footer">
+        <p>Made with <span style={heartStyle}>&#9829;</span> by Liz</p>
+        <p><a href="https://github.com/">Github</a></p>
       </footer>
     )
   }
