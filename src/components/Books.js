03fe68c2df845c1f21e04e119398d33799b5cea2
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-24 05:29:22 -0400

    fix cards 3 cols in a row

--- src/components/Books.js
+++ src/components/Books.js
@@ -10,25 +10,25 @@ const Books = (props) => {
   }
 
   return (
-    <div>
+    <Container>
+      <Row>
       {props.books && props.books.map(book =>
-        <Container>
-          <Row>
-            <Col xs={6} md={4}>
-              <Card key={book.id} className='mb-3' style={{ width: '18rem' }}>
-              <Card.Img src={book.image_url} />
-              <Card.Body>
-              <Card.Title>{book.title}</Card.Title>
-              <Card.Text>By: {book.author} {book.read} {book.rating}
-              </Card.Text>
-              <Button variant='primary' onClick={() => handleDelete(book)}>Delete</Button>
-              </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
+        <Col xs={6} md={4}>
+          <Card key={book.id} className='book-card' style={{ width: '18rem' }}>
+          <Card.Img src={book.image_url} />
+          <Card.Body>
+            <Card.Title>{book.title}</Card.Title>
+            <Card.Text>By: {book.author} <br/>
+            Read: {book.read} <br/>
+            Rating: {book.rating}
+            </Card.Text>
+            <Button variant='primary' onClick={() => handleDelete(book)}>Delete</Button>
+          </Card.Body>
+          </Card>
+        </Col>
       )}
-    </div>
+      </Row>
+    </Container>
   )
 }
 
