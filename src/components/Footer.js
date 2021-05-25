69e9b041e7955ac56b34660f1450b99030ebb526
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-25 04:41:29 -0400

    update footer from class component to functional component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import {connect} from 'react-redux';
 
 const heartStyle = {
   color: '#e25555',
 };
 
-class Footer extends React.Component {
+const Footer = () => {
 
-  render() {
-    return (
-      <footer className="footer">
-        <p>Made with <span style={heartStyle}>&#9829;</span> by Liz</p>
-        <p><a href="https://github.com/">Github</a></p>
-      </footer>
-    )
-  }
+  return (
+    <footer className="footer">
+      <p>Made with <span style={heartStyle}>&#9829;</span> by Liz</p>
+      <p><a href="https://github.com/">Github</a></p>
+    </footer>
+  )
 }
 
-export default connect()(Footer);
+export default Footer;
