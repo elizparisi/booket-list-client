1d8c5ed55ba9048c68b25feae3aa3b59027a4fcc
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-25 22:41:33 -0400

    add link to about page in navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,10 +6,11 @@ import {Navbar, Nav} from 'react-bootstrap'
 const Navigation = (props) => {
 
   return (
-      <Navbar bg="dark" variant="dark">
+      <Navbar className='navbar'>
         <Navbar.Brand href="#home">BOOKET LISTS</Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/lists">Lists</Nav.Link>
             <Nav.Link href="/lists/new">Create New List</Nav.Link>
          </Nav>
