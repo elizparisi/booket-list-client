94ec92df5e0ad1a7cafe8f4f0288b38fc8e8a4e1
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-27 18:41:21 -0400

    remove container + clean up code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import {Container, Jumbotron} from 'react-bootstrap'
+import Books from '../books.jpg'
+import {Jumbotron, Image} from 'react-bootstrap'
 
 const Home = () => {
 
   return (
-    <div>
-      <Jumbotron fluid>
-        <Container>
+    <div className='homepage'>
+        <Jumbotron fluid className='jumbotron'>
           <h1>Welcome to Booket Lists</h1>
           <h2>Create your must read lists!</h2>
-        </Container>
-      </Jumbotron>
+        </Jumbotron>
     </div>
   )
 }
