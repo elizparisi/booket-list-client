943e142a80421b6c71c13a097804d462cc397a9d
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-27 18:37:00 -0400

    rework about paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ import React from 'react'
 const About = () => {
 
   return (
-    <div>
-      <p>Is your to read list longer than a CVS receipt? Do you have pages and pages of notes with titles and authors, take screenshots of so many book covers you've lost track? Yeah, us too. That's what Booket Lists if for. You can keep track of all your must-read-books in one place. Create as many lists as you like, add books to your list and then get to reading!</p>
+    <div className='about'>
+      <p>Is your to read list longer than a CVS receipt? Do you have pages and pages of notes with titles and authors, take screenshots of so many book covers that you've lost track? Yeah, us too. That's what Booket Lists is for. Here, you can keep track of all your must-read-books in one place. Create as many lists as you want, add books to your reading list and then get to reading!</p>
     </div>
   )
 }
