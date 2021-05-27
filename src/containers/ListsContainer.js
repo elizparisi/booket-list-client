cd17c856811a84ad2996c3949acf775ddab3bb37
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-27 19:08:40 -0400

    change ListInput to ListForm

--- src/containers/ListsContainer.js
+++ src/containers/ListsContainer.js
@@ -4,7 +4,7 @@ import {Route, Switch} from 'react-router-dom'
 import {fetchLists} from '../actions/fetchLists'
 import Lists from '../components/Lists'
 import List from '../components/List'
-import ListInput from '../components/ListInput'
+import ListForm from '../components/ListForm'
 import Home from '../components/Home'
 import About from '../components/About'
 
@@ -24,7 +24,7 @@ class ListsContainer extends React.Component {
           <Route path='/about' component={About}>
             <About/>
           </Route>
-          <Route path='/lists/new' component={ListInput}/>
+          <Route path='/lists/new' component={ListForm}/>
           <Route path='/lists/:id' render={(routerProps) => <List {...routerProps} lists={this.props.lists}/>}/>
           <Route exact path='/lists' render={(routerProps) => <Lists {...routerProps} lists={this.props.lists}/>}/>
         </Switch>
